b77ef10f4892c98450e65d0eec31601dc341cba5
Ose <53968548+Osebhahiemen@users.noreply.github.com>
2025-04-16 18:13:05 +0100

    Update SampleApplicationTests.java
    Signed-off-by: Ose <53968548+Osebhahiemen@users.noreply.github.com>

--- src/test/java/io/buildpacks/example/sample/SampleApplicationTests.java
+++ src/test/java/io/buildpacks/example/sample/SampleApplicationTests.java
@@ -1,16 +1,13 @@
-package io.buildpacks.example.sample;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+@SpringBootApplication
+public class MyApplication extends SpringBootServletInitializer {  // ← Add this
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MyApplication.class);
+    }
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class SampleApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
 }
